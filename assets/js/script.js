1a30b0f3fe926305bbb0f4ae295f887623b6d2b2
Taweria <elodieali.pro@gmail.com>
2023-10-09 18:50:53 +0200

    Without reload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,39 +2,44 @@ let quoteDiv = document.querySelector(".quotes");
 let button = document.querySelector("button");
 let loader = document.querySelector(".loading");
 
-loader.style.display = "block";
+async function loadData() {
 
-setTimeout(() => {
-// fetch
-fetch(`https://thatsthespir.it/api`)
-    .then(response => response.json())
-    .then(data => {
-        // hide the loader
-        loader.style.display = "none";
-        // Create a new div for the result and display it
-        const resultDiv = document.createElement("div");
-        resultDiv.classList.add("result");
-        resultDiv.innerHTML = `
-            <p class="quote">"${data.quote}"</p>
-            <p class="author">- ${data.author}</p>
-            <img src=${data.photo ? data.photo : "assets/storage/placeholder.jpg"} alt="photo" class="img-author">
-        `;
+    loader.style.display = "block";
+    
+    setTimeout(() => {
+    // fetch
+    fetch(`https://thatsthespir.it/api`)
+        .then(response => response.json())
+        .then(data => {
+            // hide the loader
+            loader.style.display = "none";
+            // Create a new div for the result and display it
+            const resultDiv = document.createElement("div");
+            resultDiv.classList.add("result");
+            resultDiv.innerHTML = `
+                <p class="quote">"${data.quote}"</p>
+                <p class="author">- ${data.author}</p>
+                <img src=${data.photo ? data.photo : "assets/storage/placeholder.jpg"} alt="photo" class="img-author">
+            `;
+            quoteDiv.appendChild(resultDiv);
+            // Clear the input field
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            const resultDiv = document.createElement("div");
+            resultDiv.classList.add("result");
+            resultDiv.innerHTML = `
+                <p class="error"> Error fetching API </p>
+            `;
+            quoteDiv.appendChild(resultDiv);
+        });
 
-        quoteDiv.appendChild(resultDiv);
-        // Clear the input field
-    })
-    .catch(error => {
-        console.error("Error fetching data:", error);
-        const resultDiv = document.createElement("div");
-        resultDiv.classList.add("result");
-        resultDiv.innerHTML = `
-            <p class="error"> Error fetching API </p>
-        `;
-        quoteDiv.appendChild(resultDiv);
-    });
-
-}, 1000);
+    }, 1000)
+};
+loadData();
 
 button.addEventListener("click", () =>{
-    location.reload()
+    // location.reload();
+    quoteDiv.innerText = "";
+    loadData();
 })
